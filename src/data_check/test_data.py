e0bd0c7e06faad5438e2e4e47087fd4cc740d438
zuhaalfaraj <zuha.alfaraj@gmail.com>
2023-10-04 10:47:40 -0500

    update the project

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -64,8 +64,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 # Implement here test_row_count and test_price_range   #
 ########################################################
 def test_row_count(data):
+    """
+    Test the row count of the provided dataframe to ensure it lies within a specific range.
+    """
     assert 15000 < data.shape[0] < 1000000
 
-
 def test_price_range(data, min_price, max_price):
+    """
+    Test if all prices in the provided dataframe lie within the specified range.
+    """
     assert (all(data['price'].between(min_price, max_price)))
+
